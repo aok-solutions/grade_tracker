bcf81fedf40ec353caab3945cd4a76d7892144ad
Ayanga Okpokowuruk <ayanga.okpokowuruk@chime.com>
2023-12-12 22:33:11 -0600

    add/list students

--- grade_tracker.rb
+++ grade_tracker.rb
@@ -6,17 +6,37 @@ menu_prompt = "\nwould you like to: \n1) add student \n2) add assignment \n3) li
 puts "welcome to the student grade tracker"
 puts menu_prompt
 
+Student = Struct.new(:name, :assignments, :final_grade, keyword_init: true)
+student_list = []
+
+def add_student(name, students)
+	students << Student.new(name: name, assignments: [], final_grade: "Incomplete")
+end
+
+def list_students(students)
+	puts "\n"
+	students.each do |student|
+		puts "\n"
+		puts student.name
+		puts "Final Grade: #{student.final_grade}"
+		puts "\n"
+		puts "------------"
+	end
+end
+
 while (user_input = gets.chomp) != "4"
 	case user_input
 	when "1"
 		puts newline
-		puts "todo: enter student"
+		puts "enter student name"
+		student_input = gets.chomp
+		add_student(student_input, student_list)
+		list_students(student_list)
 	when "2"
 		puts newline
 		puts "todo: enter assignment"
 	when "3"
-		puts newline
-		puts "todo: list students"
+		list_students(student_list)
 	else
 		puts newline
 		puts "invalid input"
